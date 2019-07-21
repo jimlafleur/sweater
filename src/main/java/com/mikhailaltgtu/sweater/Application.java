5f32b2ab44f352c9445722b1d71404a42a0eaac4
jimlafleur <52619433+jimlafleur@users.noreply.github.com>
2019-07-22 00:59:13 +0700

    Update Application.java

--- src/main/java/com/mikhailaltgtu/sweater/Application.java
+++ src/main/java/com/mikhailaltgtu/sweater/Application.java
@@ -13,7 +13,10 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        System.out.println("Mama" + "Mila" + "Ramu");asa
+        System.out.println("Mama" + "Mila" + "Ramu");
+         System.out.println("Mama" + "Mila" + "Ramu");
+         System.out.println("Mama" + "Mila" + "Ramu");
+         System.out.println("Mama" + "Mila" + "Ramu");
     }
 
 }
